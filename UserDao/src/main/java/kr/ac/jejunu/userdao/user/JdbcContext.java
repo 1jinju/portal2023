f724e24e666c7e1586e6a6ba48419a180061b119
1jinju <gkswlswn2044@gmail.com>
2023-04-07 18:22:48 +0900

    6주차 JdbcContext

---
+++ UserDao/src/main/java/kr/ac/jejunu/userdao/user/JdbcContext.java
@@ -0,0 +1,104 @@
+package kr.ac.jejunu.userdao.user;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    private final DataSource dataSource;
+
+    public JdbcContext(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    User jdbcContextForFind(StatementStrategy statementStrategy) throws SQLException {
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        User user = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                //결과를 사용자 정보에 매핑하고
+                user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return user;
+    }
+
+    void jdbcContextForInsert(User user, StatementStrategy statementStrategy) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null; // 키 값 가져옴
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            user.setId(resultSet.getLong(1));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    void jdbcContextForUpdate(StatementStrategy statementStrategy) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- UserDao/src/main/java/kr/ac/jejunu/userdao/user/UserDao.java
+++ UserDao/src/main/java/kr/ac/jejunu/userdao/user/UserDao.java
@@ -4,128 +4,30 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    // new -> 의존성 있음
-    private final DataSource dataSource;
+    private final JdbcContext jdbcContext;
 
-    public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public UserDao(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
 
     public User findById(Long id) throws SQLException, ClassNotFoundException {
-        ResultSet resultSet = null;
-        PreparedStatement preparedStatement = null;
-        Connection connection = null;
-        User user = null;
-        try {
-            connection = dataSource.getConnection();
-            //쿼리 만들고
-            preparedStatement = connection.prepareStatement
-                    ("select id, name, password from userinfo where id = ?");
-            preparedStatement.setLong(1, id);
-            //쿼리 실행하고
-            resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                //결과를 사용자 정보에 매핑하고
-                user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setPassword(resultSet.getString("password"));
-            }
-            
-        } finally {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        //결과 리턴
-        return user;
+        StatementStrategy statementStrategy = new FindStatementStrategy(id);
+        return jdbcContext.jdbcContextForFind(statementStrategy);
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null; // 키 값 가져옴
-        try {
-            connection = dataSource.getConnection();
-            //쿼리 만들고
-            preparedStatement = connection.prepareStatement
-                    ("insert  into userinfo (name, password) values ( ?, ? )"
-                            , Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
-
-            //쿼리 실행하고
-            preparedStatement.executeUpdate();
-            resultSet = preparedStatement.getGeneratedKeys();
-            resultSet.next();
-            user.setId(resultSet.getLong(1));
-        } finally {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-    }
-
-    // refactoring, 중복 코드 extract
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        return dataSource.getConnection();
+        StatementStrategy statementStrategy = new InsertStatementStrategy(user);
+        jdbcContext.jdbcContextForInsert(user, statementStrategy);
     }
 
     public void update(User user) {
         StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
-        jdbcContextForUpdate(statementStrategy);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
     public void delete(Long id){
         StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
-        jdbcContextForUpdate(statementStrategy);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
-    private void jdbcContextForUpdate(StatementStrategy statementStrategy) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
