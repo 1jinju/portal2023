5c3510aea82ecf1c614db9b11fd8ec92012c6f13
1jinju <gkswlswn2044@gmail.com>
2023-05-19 18:03:25 +0900

    12주차 multipart resolver

--- UserDao/src/main/java/kr/ac/jejunu/userdao/user/UserController.java
+++ UserDao/src/main/java/kr/ac/jejunu/userdao/user/UserController.java
@@ -1,11 +1,20 @@
 package kr.ac.jejunu.userdao.user;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Controller
 @RequestMapping
@@ -27,6 +36,26 @@ public class UserController {
     public ModelAndView error(Exception e){
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("e", e);
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    public void upload() {
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file") MultipartFile file
+            , HttpServletRequest request) throws IOException {
+        String path = request.getServletContext().getRealPath("/") +
+                "/WEB-INF/static/" + file.getOriginalFilename();
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        BufferedOutputStream bufferedOutputStream =
+                new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url", "/images/"
+                + file.getOriginalFilename());
         return modelAndView;
     }
 }
